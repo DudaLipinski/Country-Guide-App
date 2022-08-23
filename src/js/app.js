6f321b0775842ceb1e86c41d8640060c75573b24
MariaLipinski <dudalipinski@hotmail.com>
2022-08-23 16:36:50 +0200

    feat: implement final css and field validation

--- src/js/app.js
+++ src/js/app.js
@@ -13,23 +13,41 @@ searchBtn.addEventListener("click", () => {
       let region = data[0].region + "-" + data[0].subregion;
       let capital = data[0].capital[0];
       let population = data[0].population;
-      let language = data[0].languages.por;
 
       result.innerHTML = `
         <img src="${flag}" class="flag-img">
-        <h2>${name}</h2>
+        <h2>${data[0].name.common}</h2>
         <div class="wrapper">
-          <div class="data-wrapper">
-            <h4>Region:</h4>
-            <span>${region}</span>
-            <h4>Capital:<h4>
-            <span>${capital}</span>
-            <h4>Population:<h4>
-            <span>${population}</span>
-            <h4>Language:<h4>
-            <span>${language}</span>
-          </div>
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${name}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Region:</h4>
+                <span>${region}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${capital}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${population}</span>
+            </div>
         </div>
       `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
     });
 });
